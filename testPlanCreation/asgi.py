760c1d5855e55f05515da733f4a1e9f4d879ac80
Lais da Silva Farias <laissf@cinmoto.intranet>
2020-01-07 13:58:12 -0300

    .

--- testPlanCreation/asgi.py
+++ testPlanCreation/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for helloword project.
+ASGI config for testPlanCreation project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'helloword.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testPlanCreation.settings')
 
 application = get_asgi_application()
