01c3852490996bc587f08136625bb55e8c17fdab
Lais da Silva Farias <laissf@cinmoto.intranet>
2020-01-23 15:01:01 -0300

    urls

--- polls/urls.py
+++ polls/urls.py
@@ -4,12 +4,12 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('login/', views.login_user, name='login'),
-    path('', views.login_user, name='index'),
-    path('tpinformation/', views.info_tp, name='infotp'),
+    path('login/', views.login, name='login'),
+    path('', views.login, name='index'),
+    path('info_tp/', views.info_tp, name='info_tp'),
     path('tp_filter/', views.tp_filter, name='tp_filter'),
     path('list_filter/', views.list_filter, name='list_filter'),
-    path('tcs_list/', views.tcs_list, name='tcs_list')
+    path('tcs_list/', views.tcs_list, name='tcs_list'),
 
 
 
